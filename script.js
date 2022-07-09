e95a86260e39932be1d237cf69e468737b570f1f
RafiyaMuskan11 <rafiyamuskan11@gmail.com>
2022-07-09 16:32:21 +0530

    javascript practice

--- script.js
+++ script.js
@@ -14,20 +14,46 @@ console.log(arrname);
 var newnames=new Array("umaymah","aslam");
 console.log(newnames);
 
-// to find the lenght of the array.
+// to find the lenght of an array.
 console.log(arrname.length)
 
-// sorting the array
+// sorting an array
 console.log(arrname.sort());
 
 // printing an element.
 console.log(arrname[1]);
 
 
-// adding new elements to the array.
-// push command is used to add elements.
+// push - command is used to add elements at the last of an array.
 newnames.push("mahi");
 console.log(newnames);
+
+// pop - this command is used to delete elements from the last of an array.
+
+var fruits=["apple","orange","banana","grapes"];
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+
+// shift- is used to remove or delete elements from the begining of an array.
+fruits.shift();
+
+// unshift - is used to add elements in the beginning of an array
+fruits.unshift("Lemon","Pineapple");
+
+// slice - This method returns selected elements in an array, as a new array without changing the old array.
+var subjects=["java","htlm","css","javascript","c++"];
+var webdev = subjects.slice(1, 4);
+console.log(webdev);
+
+// splice
+const fruit = ["Banana", "Orange", "Apple", "Mango"];
+
+fruit.splice(2, 0, "Lemon", "Kiwi");
+console.log(fruit);
+
+
+
 
 
 // uppercase and lowercase
@@ -44,3 +70,26 @@ var strng="RafiyaMuskan";
 console.log(strng);
 var position=strng.indexOf('M');
 console.log(position);
+
+// function
+
+function addTwoNumber(a,b){
+    var sum=0;
+    console.log(a,b,sum)
+}
+addTwoNumber(5,10);
+addTwoNumber(15,10);
+
+// function expression 
+
+const getRectArea = function(width, height) {
+    return width * height;
+  };
+  
+  console.log(getRectArea(3, 4));
+
+
+
+
+
+
